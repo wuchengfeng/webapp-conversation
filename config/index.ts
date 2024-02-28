b906aaa488f4f5bfc0650fafe0b08d9bf6691049
wuchengfeng <wuchengfeng1990@163.com>
2024-02-28 11:05:34 +0800

    更新app配置

--- config/index.ts
+++ config/index.ts
@@ -3,18 +3,18 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
-  description: '',
-  copyright: '',
+  title: '我的企业是否有机会用AI降本增效',
+  description: '红利学社认为AI应当是现阶段最大的红利，抓住AI就是抓住了时代。很多老板都很困惑，我的企业能用AI吗？试着交流一下，或许会有惊喜。',
+  copyright: '红利学社',
   privacy_policy: '',
   default_language: 'zh-Hans',
 }
 
 export const isShowPrompt = false
-export const promptTemplate = 'I want you to act as a javascript console.'
+export const promptTemplate = ''
 
 export const API_PREFIX = '/api'
 
 export const LOCALE_COOKIE_NAME = 'locale'
 
-export const DEFAULT_VALUE_MAX_LEN = 48
+export const DEFAULT_VALUE_MAX_LEN = 500000
